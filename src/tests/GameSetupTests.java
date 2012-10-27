de9325fa4bbc6a176382bb489b34a531df012041
N Harvey <nthnhrvy@gmail.com>
2012-10-26 22:20:54 -0600

    Finished setup tests

--- src/tests/GameSetupTests.java
+++ src/tests/GameSetupTests.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -15,6 +16,8 @@ import org.junit.Test;
 import org.junit.runner.Computer;
 
 import clueGame.Board;
+import clueGame.Card;
+import clueGame.Card.CardType;
 import clueGame.ComputerPlayer;
 import clueGame.Player;
 import clueGame.WalkwayCell;
@@ -22,14 +25,14 @@ import clueGame.WalkwayCell;
 public class GameSetupTests {
 	private static Board board;
 	private static Map<String,Color> names;
-	
+
 	@BeforeClass 
 	public static void setUp() throws Exception {
 		board = new Board();
 		board.loadConfigFiles("ClueBoardLegend.txt", "ClueBoardLayout.csv");
 		board.loadPlayers("players.txt");
 		board.loadCards("cards.txt");
-		
+
 		names = new HashMap<String,Color>(6);
 		names.put("Colonel Mustard",Color.yellow);
 		names.put("Professor Plum",new Color(0xFF00FF));
@@ -41,26 +44,48 @@ public class GameSetupTests {
 
 	@Test
 	public void peopleLoaded() {
-		Set<Player> opp = board.getPlayers();
-		
-		assertEquals(6, opp.size());
-		
-		if(names.remove(board.getHuman().getName())==null) fail("Your name is invalid");
-		
-		for(Player comp : opp){
-			assertTrue(comp.getName()/*message*/,names.containsKey(comp.getName()));
-			//assure all the computer players' names are in the list.
-			assertTrue("Not in walkway",board.getCellAt(comp.getCellIndex()) instanceof WalkwayCell);
-			
+		Set<Player> guests = board.getPlayers();
+
+		assertEquals(6, guests.size());
+
+		assertTrue(names.containsKey(board.getHuman().getName()));
+
+		for(Player p : guests){
+			assertTrue(p.getName()/*message*/,names.containsKey(p.getName()));
+			//assure all the players' names are in the list.
+			assertTrue("Not in walkway",board.getCellAt(p.getCellIndex()) instanceof WalkwayCell);
+
 		}
-		
+
 	}
-	
+
 	@Test
 	public void cardsLoaded() {
 		assertEquals(12+board.getRooms().size(), board.getDeck().size());
 		//6 people, 6 weapons, and however many rooms we have.
-		
+		int room=0,pers=0,weap=0;
+		for(Card c : board.getDeck()){
+			switch(c.getType()){
+			case PERSON:
+				pers++;
+				break;
+			case WEAPON:
+				weap++;
+				break;
+			case ROOM:
+				room++;
+				break;
+			default:
+				fail("Bad Card Type");
+				break;
+			}
+		}
+		assertEquals(6, pers);
+		assertEquals(6, weap);
+		assertEquals(board.getRooms().size(), room);
+		assertTrue(board.getDeck().contains(new Card("Kitchen", CardType.ROOM)));
+		assertTrue(board.getDeck().contains(new Card("Revolver", CardType.WEAPON)));
+		assertTrue(board.getDeck().contains(new Card("Coronel Mustard", CardType.PERSON)));
 	}
 
 	@Test
@@ -68,6 +93,19 @@ public class GameSetupTests {
 		board.deal();
 		assertEquals(0, board.getDeck().size());
 		//now that we dealt, the deck should be empty.
-
+		int normalNum = board.getHuman().getCards().size();
+		//assume the human has the "normal" number of cards.
+		for (Player me : board.getPlayers()){
+			assertTrue(Math.abs(normalNum-me.getCards().size())<2);//can't deviate more than 1.
+			Set<Player> others = board.getPlayers();
+			others.remove(me);
+			for(Card card : me.getCards()){
+				for (Player o : others){
+					assertFalse(o.getCards().contains(card));
+					//should not have same card as someone else.
+				}
+			}
+		}
 	}
+	
 }
