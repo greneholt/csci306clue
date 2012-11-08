5ec78777ae72912b7093fc681d9ef9acec1d44b7
Kevyn Young <keyoung@mines.edu>
2012-11-07 18:01:24 -0700

    Created DetectivePanel for human player to keep track of cards

---
+++ src/clueGame/DetectivePanel.java
@@ -0,0 +1,18 @@
+package clueGame;
+
+import java.awt.Dimension;
+
+import javax.swing.*;
+
+public class DetectivePanel extends JPanel {
+	
+	
+	public DetectivePanel() {
+		super();
+		
+		// TODO: Change the size
+		setSize(new Dimension(800, 800));
+		setMinimumSize(new Dimension(500, 500));
+		
+	}
+}
