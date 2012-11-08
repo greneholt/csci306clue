598a519e1b9494aff1a246a38268a7710c4591f5
Kevyn Young <keyoung@mines.edu>
2012-11-07 20:23:56 -0700

    Added methods to create subpanels and loaded these into the constructor

--- src/clueGame/DetectivePanel.java
+++ src/clueGame/DetectivePanel.java
@@ -1,10 +1,13 @@
 package clueGame;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.util.LinkedList;
 import java.util.List;
 
 import javax.swing.*;
+import javax.swing.border.*;
 
 public class DetectivePanel extends JPanel {
 	private List<JCheckBox> peopleCB = new LinkedList<JCheckBox>();
@@ -20,18 +23,22 @@ public class DetectivePanel extends JPanel {
 		// TODO: Change the size
 		setSize(new Dimension(800, 800));
 		setMinimumSize(new Dimension(500, 500));
-		
+		setLayout(new GridLayout(0,2));
 		// Pull in the card names and associate with checkboxes
 		peopleCB = loadCheckBoxes(Card.CardType.PERSON);
 		weaponsCB = loadCheckBoxes(Card.CardType.WEAPON);
 		roomsCB = loadCheckBoxes(Card.CardType.ROOM);
-		// Put these checkboxes into groups
-		
 		// Add appropriate cards to combo boxes
 		personGuess = createCardCombo(Card.CardType.PERSON);
 		weaponGuess = createCardCombo(Card.CardType.WEAPON);
 		roomGuess = createCardCombo(Card.CardType.ROOM);
-		// Put combo boxes into groups
+		// Add other panels
+		add(new PeoplePanel());
+		add(new PersonGuessPanel());
+		add(new WeaponsPanel());
+		add(new WeaponGuessPanel());
+		add(new RoomsPanel());
+		add(new RoomGuessPanel());
 	}
 	
 	private LinkedList<JCheckBox> loadCheckBoxes(Card.CardType cType) {
@@ -56,5 +63,56 @@ public class DetectivePanel extends JPanel {
 		}
 		guess.addItem("Unsure");
 		return guess;
+	}
+	
+	private class PeoplePanel extends JPanel {
+		public PeoplePanel() {
+			setBorder(new TitledBorder (new EtchedBorder(), "People"));
+			setLayout(new GridLayout(0,2));
+			for (JCheckBox cb : peopleCB) {
+				add(cb);
+			}
+		}
+	}
+	
+	private class WeaponsPanel extends JPanel {
+		public WeaponsPanel() {
+			setBorder(new TitledBorder (new EtchedBorder(), "Weapons"));
+			setLayout(new GridLayout(0,2));
+			for (JCheckBox cb : weaponsCB) {
+				add(cb);
+			}
+		}
+	}
+	
+	private class RoomsPanel extends JPanel {
+		public RoomsPanel() {
+			setBorder(new TitledBorder (new EtchedBorder(), "Rooms"));
+			setLayout(new GridLayout(0,2));
+			for (JCheckBox cb : roomsCB) {
+				add(cb);
+			}
+		}
+	}
+	
+	private class PersonGuessPanel extends JPanel {
+		public PersonGuessPanel() {
+			setBorder(new TitledBorder (new EtchedBorder(), "Guess Person"));
+			add(personGuess, BorderLayout.WEST);
+		}
+	}
+	
+	private class WeaponGuessPanel extends JPanel {
+		public WeaponGuessPanel() {
+			setBorder(new TitledBorder (new EtchedBorder(), "Guess Weapon"));
+			add(personGuess, BorderLayout.WEST);
+		}
+	}
+	
+	private class RoomGuessPanel extends JPanel {
+		public RoomGuessPanel() {
+			setBorder(new TitledBorder (new EtchedBorder(), "Guess Room"));
+			add(personGuess, BorderLayout.WEST);
+		}
 	}
 }
