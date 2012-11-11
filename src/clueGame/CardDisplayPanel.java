44501356f68730fb80da42cb8bc05e51db227a09
Kevyn Young <keyoung@mines.edu>
2012-11-11 13:23:50 -0700

    Updated CardDisplayPanel to be more 'modular'.

--- src/clueGame/CardDisplayPanel.java
+++ src/clueGame/CardDisplayPanel.java
@@ -1,9 +1,8 @@
 package clueGame;
 
 import java.awt.GridLayout;
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Set;
 
 import javax.swing.*;
@@ -12,9 +11,6 @@ import javax.swing.border.TitledBorder;
 
 public class CardDisplayPanel extends JPanel {
 	private Set<Card> humanCards = new HashSet<Card>();
-	private List<JTextField> people = new LinkedList<JTextField>();
-	private List<JTextField> weapons = new LinkedList<JTextField>();
-	private List<JTextField> rooms = new LinkedList<JTextField>();
 	
 	public CardDisplayPanel(Board board) {
 		super();
@@ -22,71 +18,41 @@ public class CardDisplayPanel extends JPanel {
 		humanCards = board.getHuman().getCards();
 		JLabel label = new JLabel("My Cards:");
 		add(label);
-		add(new PeoplePanel());
-		add(new WeaponsPanel());
-		add(new RoomsPanel());
+		add(new CardPanel(Card.CardType.PERSON));
+		add(new CardPanel(Card.CardType.WEAPON));
+		add(new CardPanel(Card.CardType.ROOM));
 	}
 	
-	public void loadHumanCards(){
-		// Load person cards
-		for(Card c : humanCards) {
-			if(c.getType().equals(Card.CardType.PERSON)) {
-				JTextField cField = new JTextField();
-				cField.setText(c.getName());
-				cField.setEditable(false);
-				people.add(cField);
-			}
-		}
-		// Load weapon cards
-		for(Card c : humanCards) {
-			if(c.getType().equals(Card.CardType.WEAPON)) {
-				JTextField cField = new JTextField();
-				cField.setText(c.getName());
-				cField.setEditable(false);
-				weapons.add(cField);
-			}
-		}
-		// Load room cards
-		for(Card c : humanCards) {
-			if(c.getType().equals(Card.CardType.ROOM)) {
-				JTextField cField = new JTextField();
-				cField.setText(c.getName());
-				cField.setEditable(false);
-				rooms.add(cField);
-		}
-		}
-	}
-	
-	private class PeoplePanel extends JPanel {
-		public PeoplePanel() {
+	private class CardPanel extends JPanel {
+		public CardPanel(Card.CardType type) {
 			super();
-			setBorder(new TitledBorder (new EtchedBorder(), "People"));
+			if(type.equals(Card.CardType.PERSON))
+				setBorder(new TitledBorder (new EtchedBorder(), "People"));
+			else if(type.equals(Card.CardType.WEAPON))
+				setBorder(new TitledBorder (new EtchedBorder(), "Weapons"));
+			else if(type.equals(Card.CardType.ROOM))
+				setBorder(new TitledBorder (new EtchedBorder(), "Room"));
+			else
+				return;
+			
 			setLayout(new GridLayout(0,1));
-			for(JTextField tf : people) {
+			ArrayList<JTextField> cards = loadHumanCards(type);
+			for(JTextField tf : cards) {
 				add(tf);
 			}
 		}
 	}
 	
-	private class WeaponsPanel extends JPanel {
-		public WeaponsPanel() {
-			super();
-			setBorder(new TitledBorder (new EtchedBorder(), "Weapons"));
-			setLayout(new GridLayout(0,1));
-			for(JTextField tf : weapons) {
-				add(tf);
+	public ArrayList<JTextField> loadHumanCards(Card.CardType type){
+		ArrayList<JTextField> list = new ArrayList<JTextField>();
+		for(Card c : humanCards) {
+			if(c.getType().equals(type)) {
+				JTextField cField = new JTextField();
+				cField.setText(c.getName());
+				cField.setEditable(false);
+				list.add(cField);
 			}
 		}
-	}
-	
-	private class RoomsPanel extends JPanel {
-		public RoomsPanel() {
-			super();
-			setBorder(new TitledBorder (new EtchedBorder(), "Rooms"));
-			setLayout(new GridLayout(0,1));
-			for(JTextField tf : rooms) {
-				add(tf);
-			}
-		}
+		return list;
 	}
 }
